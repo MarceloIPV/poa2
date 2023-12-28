ee4b6ada0007c9a8773632eb84994f7b2dd1ab47
Marcelo Israel Pérez Villa <74079519+MarceloIPV@users.noreply.github.com>
2023-12-28 14:22:58 -0500

    commit

--- modelosBd/poaInicial2024/cargaExcel.md.php
+++ modelosBd/poaInicial2024/cargaExcel.md.php
@@ -9,29 +9,72 @@
 
 	require_once "../../config/config2.php";
 
-	// require_once '../../PHPExcel/Classes/PHPExcel.php';
+	require_once '../../PHPExcel/Classes/PHPExcel.php';
 
-require_once '../../vendor/autoload.php';
+	$archivo = $_FILES['documentoExcel']['tmp_name'];
 
-    	
-$archivo = $_FILES['documentoExcel']['tmp_name'];
+	/*=======================================
+	=            Excel generados            =
+	=======================================*/
 
-use PhpOffice\PhpSpreadsheet\IOFactory;
+	$inputFileType = PHPExcel_IOFactory::identify($archivo);
 
-$spreadsheet = IOFactory::load($archivo);
-$sheet = $spreadsheet->getActiveSheet();
-$cellValue = $sheet->getCell('A1')->getValue();
+	$objReader = PHPExcel_IOFactory::createReader($inputFileType);
 
-foreach ($sheet->getRowIterator() as $row) {
-    foreach ($row->getCellIterator() as $cell) {
-        // $cellValue = $cell->getValue();
+	$objPHPExcel = $objReader->load($archivo);
 
-        echo $cell->getColumn()."__".$cell->getValue();
-       
-    }
-}
+	$sheet = $objPHPExcel->getSheet(0); 
+
+	$highestRow = $sheet->getHighestRow(); 
+
+	$highestColumn = $sheet->getHighestColumn();	
+	
+	/*=====  End of Excel generados  ======*/
+
+	/*====================================
+	=            Obligatorios            =
+	====================================*/
+	
+	$banderaObligatorios=false;
+	$obligatorios__exceles=array();
+
+	
+	/*=====  End of Obligatorios  ======*/
+	
+	/*====================================
+	=            Código items            =
+	====================================*/
+	
+	switch ($tipo) {
 
 
+		case "act__administrativas":
+
+			$tipo__array=array();
+			$nombre__array=array();
+			$luz__array=array();
+			$agua__array=array();
+
+			for ($row = 2; $row <= $highestRow; $row++){ 
+
+				array_push($tipo__array, trim($sheet->getCell("A".$row)->getValue()));
+				array_push($nombre__array, trim($sheet->getCell("B".$row)->getValue()));
+				
+
+			}
 
 
-	// echo json_encode($jason);
+			$jason['tipo__array']=$tipo__array;
+			$jason['nombre__array']=$nombre__array;
+			
+			
+
+		break;
+
+
+	}
+
+	$jason['banderaObligatorios']=$banderaObligatorios;
+
+
+	echo json_encode($jason);
